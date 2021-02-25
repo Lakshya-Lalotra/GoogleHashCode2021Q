aa2473f3c12098cbfbd87279916fb30b3aa683be
Lakshya Singh Lalotra <68769782+Lakshya-Lalotra@users.noreply.github.com>
2021-02-25 23:43:23 +0530

    Main file

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <memory>
+#include <string>
+#include <vector>
+
+#define endl "\n"
+
+using namespace std;
+
+int main ()
+{
+    freopen("inputs/input.txt", "r", stdin);
+    freopen("outputs/output.txt", "w", stdout);
+
+    cout << "Hello, world!" << endl;
+
+    return 0;
+}
