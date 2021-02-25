283de24288d7edfa55a4235f5cb647427a347452
Lakshya Singh Lalotra <68769782+Lakshya-Lalotra@users.noreply.github.com>
2021-02-26 00:17:53 +0530

    Updated

--- main.cpp
+++ main.cpp
@@ -3,8 +3,35 @@
 #include <memory>
 #include <string>
 #include <vector>
+#include 
+#include "street.h"
 
 #define endl "\n"
+
+
+typedef pair<int, int> Pair;
+
+// Represent a graph object
+
+class streetGraph{
+public:
+    vector<vector<Pair>> adjList;
+
+    StreetGraph(vector<Street> const &streets, int N)
+    {
+        // resizing vector to hold n element in vector<pair>
+        adjList.resize(N);
+
+        for (auto &street: streets)
+        {
+            int src = street.src;
+            int dest = street.dest;
+            int weight = street.weight;
+
+            
+            adjList[src].push_back(make_pair(dest, weight));
+        }
+    }
 
 using namespace std;
 
@@ -16,6 +43,16 @@ using namespace std;
                     { 0, 1, 6 }, { 1, 2, 7 }, { 2, 0, 5 }, { 2, 1, 4 },
                     { 3, 2, 10 }, { 5, 4, 1 }, { 4, 5, 3 }
             };
+    
+     int N = 6;
+
+  
+    Graph graph(edges, N);
+
+
+    printGraph(graph, N);
+
+   
 
     return 0;
 }
